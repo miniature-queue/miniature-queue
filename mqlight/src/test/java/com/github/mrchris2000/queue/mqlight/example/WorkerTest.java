3f8600bc77b8e27f0636b5ca3aecf4abebb0c208
Chris Haggan <chaggan@gmail.com>
2016-05-19 17:19:40 +0100

    Worker Test updated to work against Docker instance

--- mqlight/src/test/java/com/github/mrchris2000/queue/mqlight/example/WorkerTest.java
+++ mqlight/src/test/java/com/github/mrchris2000/queue/mqlight/example/WorkerTest.java
@@ -20,10 +20,11 @@ public class WorkerTest {
             DockerRule.builder()
                     .imageName("ibmcom/mqlight:1.0")
                     .env("LICENSE","accept")
+                    .env("volume", "/var/example:/var/mqlight")
                     .env("MQLIGHT_USER","user")
                     .env("MQLIGHT_PASSWORD","password")
                     .publishAllPorts(true)
-                    .waitForMessage("Running in standalone mode")
+                    .waitForMessage("Monitoring MQ Light...", 90)
                     .build();
 
     @Queue(value = "/worker-example", queueTypeHint = QueueType.WORKER_QUEUE)
@@ -40,8 +41,8 @@ public class WorkerTest {
         final AtomicBoolean oneReceiveMessage = new AtomicBoolean(false);
         final AtomicBoolean twoReceiveMessage = new AtomicBoolean(false);
 
-        MqLightServer mqls =  new MqLightServer("amqp://user:password@"+ dockerRule.getDockerHost());
-        MqLightServer mqls2 =  new MqLightServer("amqp://user:password@"+ dockerRule.getDockerHost());
+        MqLightServer mqls =  new MqLightServer("amqp://user:password@"+ dockerRule.getDockerHost()+":"+dockerRule.getExposedContainerPort("5672"));
+        MqLightServer mqls2 =  new MqLightServer("amqp://user:password@"+ dockerRule.getDockerHost()+":"+dockerRule.getExposedContainerPort("5672"));
 
         WorkerExampleQueue one = Queuify.builder().decoder(new StringDecoder()).server(mqls).target(WorkerExampleQueue.class);
         WorkerExampleQueue two = Queuify.builder().decoder(new StringDecoder()).server(mqls2).target(WorkerExampleQueue.class);
@@ -51,7 +52,7 @@ public class WorkerTest {
         one.receiveMessage((x) -> { oneReceiveMessage.set(true); return true; });
         two.receiveMessage((x) -> { twoReceiveMessage.set(true); return true; });
 
-
+        Thread.sleep(5000);
         sender.publishMessage("msg");
         Thread.sleep(500L);
 
